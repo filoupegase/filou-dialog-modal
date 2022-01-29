e2a6b952640523853ea4c9f2a440a450ce4465cf
filoupegase <loisoncorentin@yahoo.fr>
2022-01-29 15:59:53 +0100

    feat/personnal_dialog

--- src/lib/components/AlertDialog.js
+++ src/lib/components/AlertDialog.js
@@ -7,42 +7,26 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
 
-export default function AlertDialog() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+export default function AlertDialog(props) {
+  const { title, description, btnCloseContent, open, onCancel } = props;
   return (
       <>
-        <Button variant="outlined" onClick={ handleClickOpen }>
-          Open alert dialog
-        </Button>
         <Dialog
             open={ open }
-            onClose={ handleClose }
+            onClose={ onCancel }
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
         >
           <DialogTitle id="alert-dialog-title">
-            { 'Use Google\'s location service?' }
+            { title }
           </DialogTitle>
           <DialogContent>
             <DialogContentText id="alert-dialog-description">
-              Let Google help apps determine location. This means sending anonymous
-              location data to Google, even when no apps are running.
+              { description }
             </DialogContentText>
           </DialogContent>
           <DialogActions>
-            <Button onClick={ handleClose }>Disagree</Button>
-            <Button onClick={ handleClose } autoFocus>
-              Agree
-            </Button>
+            <Button onClick={ onCancel }>{ btnCloseContent }</Button>
           </DialogActions>
         </Dialog>
       </>
